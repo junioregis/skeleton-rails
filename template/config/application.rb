82574451d4393b93c537c07f7d555327307b4976
Regis Junior <junioregis@gmail.com>
2019-08-28 23:18:17 -0300

    [update] rails 6

--- template/config/application.rb
+++ template/config/application.rb
@@ -8,6 +8,8 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 
@@ -17,7 +19,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.api_only = true
     config.i18n.default_locale = 'pt-BR'
 
